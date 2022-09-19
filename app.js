ebc97d5c6efb4755417ccdf55ef2b60ee4379f53
ivan2798 <inge1998@hotmail.com>
2022-09-19 09:42:49 -0500

    Ambito de bloque y ambito global

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+//Esto es un bloque- Comentario de una linea
+/*
+Esto 
+es 
+un 
+comentario
+de
+varias
+lineas 
+*/
+
 //var global
 var hola = "Hola mundo";
 //let bloque
 let hello = "Hello world";
 console.log(hola);
-//ambito de bloque
-if(true){
-    var a = 3;
+console.log(hello);
+console.log(window);
+console.log(window.hola);
+console.log(window.hello);
+//Ambito de bloque
+var musica = "Rock";
+console.log("Variable Música antes del bloque",musica);
+
+{
+  var musica="Pop";
+  console.log("Variable Música dentro del bloque",musica);
+
 }
+
+console.log("Variable Música fuera del bloque",musica);
+
+console.log("************************************");
+let musica2 = "Rock";
+console.log("Variable Música antes del bloque",musica2);
+
+{
+  let musica2="Pop";
+  console.log("Variable Música dentro del bloque",musica);
+
+}
+
+console.log("Variable Música fuera del bloque",musica2);
